58a9e9d1fc59acd35ad339b048992f505ea59ee3
Yuzu-Lemon0822 <yuzulemon140416@gmail.com>
2026-02-10 17:58:18 +0900

    tmp

--- main/display.js
+++ main/display.js
@@ -2,6 +2,17 @@ import { ctx } from "../main.js"
 
 export const pen = class {
   constructor() {
+    canvasData:{
+      centerPos:[0,0]
+      
+    }
+    dwon:false
+    pos:[]
+  }
+  geom() {
+    
+  }
+  rect() {
 
   }
 }
