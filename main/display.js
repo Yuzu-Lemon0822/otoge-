e03e8f3b5664db3c02c649cf45390450f74eebf4
Yuzu-Lemon0822 <yuzulemon140416@gmail.com>
2026-02-10 23:14:59 +0900

    tmp

--- main/display.js
+++ main/display.js
@@ -1,18 +1,40 @@
 import { ctx } from "../main.js"
 
-export const pen = class {
-  constructor() {
-    canvasData:{
-      centerPos:[0,0]
-      
-    }
-    dwon:false
-    pos:[]
-  }
-  geom() {
-    
-  }
-  rect() {
+const canvasData = {}
 
+const math = {
+  convert (v) {
+    return Array.isArray(v) ? v : [v, v];
+  },
+
+  calc (v1, v2, op) {
+    const [a, b] = this.convert(v1);
+    const [c, d] = this.convert(v2);
+    return [op(a, c), op(b, d)];
+  },
+
+  add(v1, v2) { return this.calc(v1, v2, (a, b) => a + b); },
+  sub(v1, v2) { return this.calc(v1, v2, (a, b) => a - b); },
+  mul(v1, v2) { return this.calc(v1, v2, (a, b) => a * b); },
+  div(v1, v2) { return this.calc(v1, v2, (a, b) => a / b); },
+};
+
+function newPen (Id = `pen${Object.keys(canvasData).length}`, opts = {}) {
+  const data = {
+    down: false,
+    pos: [0,0] ?? opts.pos,
+    color: "#000000" ?? opts.color,
+    size: 1 ?? opts.size,
   }
+  canvasData[Id] = data;
+}
+
+newPen() //初期で一本装備
+
+export function pen(Id = "pen0") {
+  return {
+    set(pos) {
+      canvasData
+    }
+  };
 }
